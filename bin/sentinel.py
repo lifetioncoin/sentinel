ed1fe28e4459391de41b3c900abfd9a784d2a48b
Charles Wire <57906278+charleswire@users.noreply.github.com>
2020-06-05 18:10:54 +0700

    Update sentinel.py

--- bin/sentinel.py
+++ bin/sentinel.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib
 import init
 import config
 import misc
-from dashd import DashDaemon
+from lifetioncoind import LifetionCoinDaemon
 from models import Superblock, Proposal, GovernanceObject
 from models import VoteSignals, VoteOutcomes, Transient
 import socket
@@ -19,21 +19,21 @@ from scheduler import Scheduler
 import argparse
 
 
-# sync dashd gobject list with our local relational DB backend
-def perform_dashd_object_sync(dashd):
-    GovernanceObject.sync(dashd)
+# sync lifetioncoind gobject list with our local relational DB backend
+def perform_lifetioncoind_object_sync(lifetioncoind):
+    GovernanceObject.sync(lifetioncoind)
 
 
-def prune_expired_proposals(dashd):
+def prune_expired_proposals(lifetioncoind):
     # vote delete for old proposals
-    for proposal in Proposal.expired(dashd.superblockcycle()):
-        proposal.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+    for proposal in Proposal.expired(lifetioncoind.superblockcycle()):
+        proposal.vote(lifetioncoind, VoteSignals.delete, VoteOutcomes.yes)
 
 
-def attempt_superblock_creation(dashd):
-    import dashlib
+def attempt_superblock_creation(lifetioncoind):
+    import lifetioncoinlib
 
-    if not dashd.is_masternode():
+    if not lifetioncoind.is_masternode():
         print("We are not a Masternode... can't submit superblocks!")
         return
 
@@ -44,7 +44,7 @@ def attempt_superblock_creation(dashd):
     # has this masternode voted on *any* superblocks at the given event_block_height?
     # have we voted FUNDING=YES for a superblock for this specific event_block_height?
 
-    event_block_height = dashd.next_superblock_height()
+    event_block_height = lifetioncoind.next_superblock_height()
 
     if Superblock.is_voted_funding(event_block_height):
         # printdbg("ALREADY VOTED! 'til next time!")
@@ -52,20 +52,20 @@ def attempt_superblock_creation(dashd):
         # vote down any new SBs because we've already chosen a winner
         for sb in Superblock.at_height(event_block_height):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.funding, VoteOutcomes.no)
+                sb.vote(lifetioncoind, VoteSignals.funding, VoteOutcomes.no)
 
         # now return, we're done
         return
 
-    if not dashd.is_govobj_maturity_phase():
+    if not lifetioncoind.is_govobj_maturity_phase():
         printdbg("Not in maturity phase yet -- will not attempt Superblock")
         return
 
-    proposals = Proposal.approved_and_ranked(proposal_quorum=dashd.governance_quorum(), next_superblock_max_budget=dashd.next_superblock_max_budget())
-    budget_max = dashd.get_superblock_budget_allocation(event_block_height)
-    sb_epoch_time = dashd.block_height_to_epoch(event_block_height)
+    proposals = Proposal.approved_and_ranked(proposal_quorum=lifetioncoind.governance_quorum(), next_superblock_max_budget=lifetioncoind.next_superblock_max_budget())
+    budget_max = lifetioncoind.get_superblock_budget_allocation(event_block_height)
+    sb_epoch_time = lifetioncoind.block_height_to_epoch(event_block_height)
 
-    sb = dashlib.create_superblock(proposals, event_block_height, budget_max, sb_epoch_time)
+    sb = lifetioncoinlib.create_superblock(proposals, event_block_height, budget_max, sb_epoch_time)
     if not sb:
         printdbg("No superblock created, sorry. Returning.")
         return
@@ -73,12 +73,12 @@ def attempt_superblock_creation(dashd):
     # find the deterministic SB w/highest object_hash in the DB
     dbrec = Superblock.find_highest_deterministic(sb.hex_hash())
     if dbrec:
-        dbrec.vote(dashd, VoteSignals.funding, VoteOutcomes.yes)
+        dbrec.vote(lifetioncoind, VoteSignals.funding, VoteOutcomes.yes)
 
         # any other blocks which match the sb_hash are duplicates, delete them
         for sb in Superblock.select().where(Superblock.sb_hash == sb.hex_hash()):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+                sb.vote(lifetioncoind, VoteSignals.delete, VoteOutcomes.yes)
 
         printdbg("VOTED FUNDING FOR SB! We're done here 'til next superblock cycle.")
         return
@@ -86,24 +86,24 @@ def attempt_superblock_creation(dashd):
         printdbg("The correct superblock wasn't found on the network...")
 
     # if we are the elected masternode...
-    if (dashd.we_are_the_winner()):
+    if (lifetioncoind.we_are_the_winner()):
         printdbg("we are the winner! Submit SB to network")
-        sb.submit(dashd)
+        sb.submit(lifetioncoind)
 
 
-def check_object_validity(dashd):
+def check_object_validity(lifetioncoind):
     # vote (in)valid objects
     for gov_class in [Proposal, Superblock]:
         for obj in gov_class.select():
-            obj.vote_validity(dashd)
+            obj.vote_validity(lifetioncoind)
 
 
-def is_dashd_port_open(dashd):
+def is_lifetioncoind_port_open(lifetioncoind):
     # test socket open before beginning, display instructive message to MN
     # operators if it's not
     port_open = False
     try:
-        info = dashd.rpc_command('getgovernanceinfo')
+        info = lifetioncoind.rpc_command('getgovernanceinfo')
         port_open = True
     except (socket.error, JSONRPCException) as e:
         print("%s" % e)
@@ -112,26 +112,26 @@ def is_dashd_port_open(dashd):
 
 
 def main():
-    dashd = DashDaemon.from_dash_conf(config.dash_conf)
+    lifetioncoind = LifetionCoinDaemon.from_lifetioncoin_conf(config.lifetioncoin_conf)
     options = process_args()
 
     # print version and return if "--version" is an argument
     if options.version:
-        print("Dash Sentinel v%s" % config.sentinel_version)
+        print("LifetionCoin Sentinel v%s" % config.sentinel_version)
         return
 
-    # check dashd connectivity
-    if not is_dashd_port_open(dashd):
-        print("Cannot connect to dashd. Please ensure dashd is running and the JSONRPC port is open to Sentinel.")
+    # check lifetioncoind connectivity
+    if not is_lifetioncoind_port_open(lifetioncoind):
+        print("Cannot connect to lifetioncoind. Please ensure lifetioncoind is running and the JSONRPC port is open to Sentinel.")
         return
 
-    # check dashd sync
-    if not dashd.is_synced():
-        print("dashd not synced with network! Awaiting full sync before running Sentinel.")
+    # check lifetioncoind sync
+    if not lifetioncoind.is_synced():
+        print("lifetioncoind not synced with network! Awaiting full sync before running Sentinel.")
         return
 
     # ensure valid masternode
-    if not dashd.is_masternode():
+    if not lifetioncoind.is_masternode():
         print("Invalid Masternode Status, cannot continue.")
         return
 
@@ -163,16 +163,16 @@ def main():
     # ========================================================================
     #
     # load "gobject list" rpc command data, sync objects into internal database
-    perform_dashd_object_sync(dashd)
+    perform_lifetioncoind_object_sync(lifetioncoind)
 
     # auto vote network objects as valid/invalid
-    # check_object_validity(dashd)
+    # check_object_validity(lifetioncoind)
 
     # vote to delete expired proposals
-    prune_expired_proposals(dashd)
+    prune_expired_proposals(lifetioncoind)
 
     # create a Superblock if necessary
-    attempt_superblock_creation(dashd)
+    attempt_superblock_creation(lifetioncoind)
 
     # schedule the next run
     Scheduler.schedule_next_run()
@@ -196,7 +196,7 @@ def process_args():
                         dest='bypass')
     parser.add_argument('-v', '--version',
                         action='store_true',
-                        help='Print the version (Dash Sentinel vX.X.X) and exit')
+                        help='Print the version (LifetionCoin Sentinel vX.X.X) and exit')
 
     args = parser.parse_args()
 
