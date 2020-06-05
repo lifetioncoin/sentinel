54df228a5009b44dcc1bff7f43b27cc6848b77b6
Charles Wire <57906278+charleswire@users.noreply.github.com>
2020-06-05 18:01:06 +0700

    Update travis_setup.sh

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -evx
 
-mkdir ~/.dashcore
+mkdir ~/.lifetioncoincore
 
 # safety check
-if [ ! -f ~/.dashcore/.dash.conf ]; then
-  cp share/dash.conf.example ~/.dashcore/dash.conf
+if [ ! -f ~/.lifetioncoincore/.lifetioncoin.conf ]; then
+  cp share/lifetioncoin.conf.example ~/.lifetioncoincore/lifetioncoin.conf
 fi
